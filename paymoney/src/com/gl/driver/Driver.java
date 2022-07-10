f7b1d77a1c101198b07026115d86f2b71a16393d
harishsharmahn <52082301+harishsharmahn@users.noreply.github.com>
2022-07-10 23:05:12 +0530

    Add files via upload

---
+++ paymoney/src/com/gl/driver/Driver.java
@@ -0,0 +1,28 @@
+package com.gl.driver;
+
+import java.util.Scanner;
+
+import com.gl.service.PayMoneyProblem;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		int noOfTranasactions = 0;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("enter the size of transaction array");
+		noOfTranasactions = scan.nextInt();
+		
+		int transactionAmounts[] = new int[noOfTranasactions];
+		
+		System.out.println("enter the values of array");
+		for(int index=0; index< transactionAmounts.length; index++) {
+			transactionAmounts[index] = scan.nextInt();
+		}
+		
+		PayMoneyProblem pmp = new PayMoneyProblem();
+		pmp.transaction(transactionAmounts);
+		scan.close();
+	}
+
+}
