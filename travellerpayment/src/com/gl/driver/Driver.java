e9477e4506c4b858621f3333413e5ca10500f925
harishsharmahn <52082301+harishsharmahn@users.noreply.github.com>
2022-07-11 01:13:20 +0530

    Add files via upload

---
+++ travellerpayment/src/com/gl/driver/Driver.java
@@ -0,0 +1,27 @@
+package com.gl.driver;
+import java.util.Scanner;
+import com.gl.service1.MergeSort;
+import com.gl.service1.TravellerPayment;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("How many currency denominations ");
+		int  noOfDenominations = scan.nextInt();
+		int denominations[] = new int[noOfDenominations];
+		TravellerPayment tpay = new TravellerPayment();
+		tpay.getDenominations(denominations);
+		
+
+		MergeSort mergeSort = new MergeSort();
+		mergeSort.sortDsc(denominations, 0, denominations.length -1);
+		
+		System.out.println("Enter the amount you want to pay ");
+		int amountToPay = scan.nextInt();
+			
+		tpay.denominationsToPay(denominations, amountToPay);
+		scan.close();
+		
+	}
+}
